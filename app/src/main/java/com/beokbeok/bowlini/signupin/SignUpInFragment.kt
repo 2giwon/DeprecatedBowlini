ccd36bbe499320251d8cf7905c5f6af6b3c92354
kekemusa <kekemusa37@naver.com>
2019-07-13 00:01:00 +0900

    [Firebase][SignUpIn] GoogleSignInClient 설정

--- app/src/main/java/com/beokbeok/bowlini/signupin/SignUpInFragment.kt
+++ app/src/main/java/com/beokbeok/bowlini/signupin/SignUpInFragment.kt
@@ -1,11 +1,23 @@
 package com.beokbeok.bowlini.signupin
 
 
-import androidx.fragment.app.Fragment
 import com.beokbeok.bowlini.R
 import com.beokbeok.bowlini.base.BaseFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
-/**
- * A simple [Fragment] subclass.
- */
-class SignUpInFragment : BaseFragment(R.layout.fragment_sign_up_in)
+class SignUpInFragment : BaseFragment(R.layout.fragment_sign_up_in) {
+
+    private val googleSignInClient: GoogleSignInClient? by lazy {
+        activity?.let {
+            GoogleSignIn.getClient(
+                it,
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(it.getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+            )
+        }
+    }
+}
