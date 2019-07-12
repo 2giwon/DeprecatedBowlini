7ee684baa42340cdb82521e849baa017d5db4a76
kekemusa <kekemusa37@naver.com>
2019-07-12 13:14:44 +0900

    [SignUpIn] Fragment 추가

---
+++ app/src/main/java/com/beokbeok/bowlini/signupin/SignUpInFragment.kt
@@ -0,0 +1,6 @@
+package com.beokbeok.bowlini.signupin
+
+import com.beokbeok.bowlini.R
+import com.beokbeok.bowlini.base.BaseFragment
+
+class SignUpInFragment : BaseFragment(R.layout.fragment_sign_up_in)
