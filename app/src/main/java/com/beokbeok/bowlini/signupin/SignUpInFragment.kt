0d188f753f261da5de1bab8c99f8a20f53764309
kekemusa <kekemusa37@naver.com>
2019-07-15 23:38:23 +0900

    [Firebase][SignUpIn] 로그인 후 onActivityResult 콜백 시 처리 로직 추가

--- app/src/main/java/com/beokbeok/bowlini/signupin/SignUpInFragment.kt
+++ app/src/main/java/com/beokbeok/bowlini/signupin/SignUpInFragment.kt
@@ -1,6 +1,7 @@
 package com.beokbeok.bowlini.signupin
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.beokbeok.bowlini.R
@@ -10,15 +11,26 @@ import com.beokbeok.bowlini.databinding.FragmentSignUpInBinding
 class SignUpInFragment : BaseFragment<FragmentSignUpInBinding>(
     R.layout.fragment_sign_up_in
 ) {
+    private lateinit var vm: SignUpInViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initBinding()
     }
 
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        if (requestCode == RC_SIGN_IN) {
+            vm.firebaseAuthWithGoogle(data)
+        }
+    }
+
     private fun initBinding() {
         context?.let {
-            val vm = SignUpInViewModel(it)
+            vm = SignUpInViewModel(it)
             binding.vm = vm
             binding.lifecycleOwner = this
             vm.activityToStart.observe(
