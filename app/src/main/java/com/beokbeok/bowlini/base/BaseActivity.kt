dbd2ff3a52a300def2026f5c16999ef6e20164eb
kekemusa <kekemusa37@naver.com>
2019-08-07 16:18:46 +0900

    [BUG] 빌드 에러 수정

--- app/src/main/java/com/beokbeok/bowlini/base/BaseActivity.kt
+++ app/src/main/java/com/beokbeok/bowlini/base/BaseActivity.kt
@@ -3,14 +3,21 @@ package com.beokbeok.bowlini.base
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity(
+abstract class BaseActivity<VDB : ViewDataBinding>(
     @LayoutRes
     private val layoutResID: Int
 ) : AppCompatActivity() {
 
+    protected lateinit var binding: VDB
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutResID)
+        binding = DataBindingUtil.setContentView(
+            this,
+            layoutResID
+        )
     }
 }
